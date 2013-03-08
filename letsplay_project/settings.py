df366178ee7f974b7116ea1d0855b628cb151692
Scott McFarlane <scott@scobuntu.com>
2013-03-08 19:09:38 +0000

    Adding Flatpages app
    Flatpages is useful for creating pages that don't really change much e.g
    About Page. Check /admin to add a flatpage.

--- letsplay_project/settings.py
+++ letsplay_project/settings.py
@@ -94,6 +94,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
 ROOT_URLCONF = 'letsplay_project.urls'
@@ -108,6 +109,7 @@ TEMPLATE_DIRS = (
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
+    'django.contrib.flatpages',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
