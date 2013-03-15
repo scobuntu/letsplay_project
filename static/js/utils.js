3c0c2bede1fdaa7fab7374b92c5b18d79129321b
Scott McFarlane <scott@scobuntu.com>
2013-03-15 01:44:59 +0000

    Removed unused files, validated home page

--- static/js/utils.js
+++ static/js/utils.js
@@ -4,6 +4,36 @@ $(document).ready(function(){
 	$('#welcomeModal').modal('show');
 	$('#welcomeModal').fadeIn();
     }, 700);
+    
+// Hide input on click
+function hideFormText() {
+	var _inputs = document.getElementsByTagName('input');
+	var _txt = document.getElementsByTagName('textarea');
+	var _value = [];
+	
+	if (_inputs) {
+		for(var i=0; i<_inputs.length; i++) {
+			if (_inputs[i].type == 'text' || _inputs[i].type == 'password') {
+				
+				_inputs[i].index = i;
+				_value[i] = _inputs[i].value;
+				
+				_inputs[i].onfocus = function(){
+					if (this.value == _value[this.index])
+						this.value = '';
+				}
+				_inputs[i].onblur = function(){
+					if (this.value == '')
+						this.value = _value[this.index];
+				}
+			}
+		}
+	}
+}
+if (window.addEventListener)
+	window.addEventListener("load", hideFormText, false);
+else if (window.attachEvent)
+	window.attachEvent("onload", hideFormText);
 });
 // Form functionality
 $(".checkboxes input:checkbox:first").change(function () {
