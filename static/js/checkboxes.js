6cfe74cf4bb5d8ac01be756fcbecef145badac84
scobuntu <scott@scobuntu.com>
2013-03-15 14:19:24 +0000

    js fixes and template logic changes

--- static/js/checkboxes.js
+++ static/js/checkboxes.js
@@ -2,5 +2,4 @@
 $(".checkboxes input:checkbox:first").change(function () {
 $(".checkboxes").find(':checkbox').prop("checked", this.checked);
 });
-$('#avatars ul li input:radio:eq(0)').attr('checked', 'checked');
-$('#backgrounds ul li input:radio:eq(0)').attr('checked', 'checked');
+$('#avatars ul li input:radio:eq(0), #backgrounds ul li input:radio:eq(0)').attr('checked', 'checked');
