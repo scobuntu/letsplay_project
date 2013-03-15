7954b38505ea129f06af7e0360aba4c2a752b6c2
Scott McFarlane <scott@scobuntu.com>
2013-03-15 02:33:14 +0000

    removed unused files

--- letsplay_project/settings.py
+++ letsplay_project/settings.py
@@ -54,10 +54,6 @@ MEDIA_ROOT = ''
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
 MEDIA_URL = ''
 
-FIXTURE_DIRS = (
-   project_path('letsplay_project/letsplay/fixtures'),
-)
-
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
@@ -115,8 +111,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-	'letsplay',
-    #'compressor',
+    'letsplay',
 )
 
 TEST_RUNNER = 'discover_runner.DiscoverRunner'
